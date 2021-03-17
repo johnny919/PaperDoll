e8ca64c57fa1109949ba3c4b3bcc8bf7754de7ba
Johnny <swen1110@gmail.com>
2021-03-17 15:58:25 +0800

    first commit

--- PaperDoll/ViewController.swift
+++ PaperDoll/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var glassesUIImageView: UIImageView!
+    @IBOutlet weak var bodyUIImageView: UIImageView!
+    @IBOutlet weak var faceUIImageView: UIImageView!
+    @IBOutlet weak var headUIImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func glasses(_ sender: UIButton) {
+        glassesUIImageView.image = sender.currentImage
+    }
+    @IBAction func body(_ sender: UIButton) {
+        bodyUIImageView.image = sender.currentImage
+    }
+    @IBAction func head(_ sender: UIButton) {
+        headUIImageView.image = sender.currentImage
+    }
+    
+    @IBAction func face(_ sender: UIButton) {
+        faceUIImageView.image=sender.currentImage
+    }
 }
 
